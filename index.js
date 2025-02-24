cf91ef997d4835155caf07212970122e96a54245
Golu_singh <637golusingh@gmail.com>
2025-02-24 16:58:14 +0530

    uopdate

--- index.js
+++ index.js
@@ -7,7 +7,21 @@ const playroutes = require('./routes/play.route');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+const allowedOrigins = [
+  'https://personal-manager-nine.vercel.app',
+  'http://localhost:5173'
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
 app.use(express.json());
 
 connectDB();
